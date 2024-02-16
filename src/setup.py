79449364110302b4bfa27fb641e5fd6d159d8cca
steveyuwono <steveyuwono@gmail.com>
2024-02-16 13:32:22 +0100

    Refact: New Repo for Gymnasium #900

--- src/setup.py
+++ src/setup.py
@@ -1,22 +1,19 @@
 from setuptools import setup
 
 
-setup(name='mypackage',
-version='1.0.0',
-description='MLPro: Integration River',
+setup(name='mlpro-int-gymnasium',
+version='0.1.0',
+description='MLPro: Integration Gymnasium',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
 license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
-packages=['mypackage'],
+packages=['mlpro_int_gymnasium'],
 
 # Package dependencies for full installation
 extras_require={
     "full": [
-        "dill",
-        "numpy",
-        "matplotlib",
-        "multiprocess",
-        "mlpro"
+        "mlpro[full]>=1.3.1",
+        "gymnasium[all]>=0.29"
     ],
 },
 
