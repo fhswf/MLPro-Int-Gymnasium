297f6797593f699a5fdbc3e68b2e72cc8f36bf2c
steveyuwono <steveyuwono@gmail.com>
2024-02-24 21:57:47 +0100

    test

--- src/setup.py
+++ src/setup.py
@@ -13,8 +13,7 @@ packages=['mlpro_int_gymnasium'],
 extras_require={
     "full": [
         "mlpro[full]>=1.3.1",
-        "gymnasium[all]>=0.29",
-        "pandas>=2.1.3"
+        "gymnasium[all]>=0.29"
     ],
 },
 
