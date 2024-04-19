62e6e000927d9a5029c93e18bacf5965ab877a33
detlefarend <detlef@arend-privat.net>
2024-04-19 07:11:58 +0200

    Preparation of v1.0.0

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro-int-gymnasium',
-version='0.1.3',
+version='1.0.0',
 description='MLPro: Integration Gymnasium',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
@@ -12,7 +12,7 @@ packages=['mlpro_int_gymnasium'],
 # Package dependencies for full installation
 extras_require={
     "full": [
-        "mlpro[full]>=1.3.1",
+        "mlpro[full]>=1.4.0",
         "gymnasium[all]>=0.29"
     ],
 },
