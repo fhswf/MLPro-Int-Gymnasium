133b32fce7961a43734f4aba04c57523262748c7
detlefarend <detlef@arend-privat.net>
2024-04-21 15:16:19 +0200

    Preparation of v1.0.1

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro-int-gymnasium',
-version='1.0.0',
+version='1.0.1',
 description='MLPro: Integration Gymnasium',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
