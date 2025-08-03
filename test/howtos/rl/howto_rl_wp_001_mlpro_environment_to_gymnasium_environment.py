27d734b6648c5d48965c0cbce807994374fc4d14
Steve Yuwono <88610818+steveyuwono@users.noreply.github.com>
2025-08-03 19:49:05 +0200

    Update howto_rl_wp_001_mlpro_environment_to_gymnasium_environment.py

--- test/howtos/rl/howto_rl_wp_001_mlpro_environment_to_gymnasium_environment.py
+++ test/howtos/rl/howto_rl_wp_001_mlpro_environment_to_gymnasium_environment.py
@@ -34,7 +34,11 @@ You will learn:
 """
 
 
-from mlpro.bf.various import Log
+from mlpro.bf import *
+from mlpro.bf.math import *
+from mlpro.bf.systems import *
+from mlpro.bf.plot import *
+import numpy as np
 from mlpro_int_gymnasium.wrappers import WrEnvMLPro2GYM
 from mlpro.rl.pool.envs.gridworld import GridWorld
 from gymnasium.utils.env_checker import check_env
